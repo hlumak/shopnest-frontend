a7f9efe31354918dda46418344c3bd8fba17e02f
hlumak <vglumakov@gmail.com>
2025-05-12 00:27:33 +0300

    add removing from cart

--- src/components/layouts/main-layout/header/header-menu/header-cart/cart-item/CartActions.tsx
+++ src/components/layouts/main-layout/header/header-menu/header-cart/cart-item/CartActions.tsx
@@ -1,32 +1,35 @@
-import { Minus, Plus } from 'lucide-react'
+import { Minus, Plus } from 'lucide-react';
 
-import { Button } from '@/components/ui/Button'
+import { Button } from '@/components/ui/Button';
 
-import { useActions } from '@/hooks/useActions'
-import { useCart } from '@/hooks/useCart'
+import { useActions } from '@/hooks/useActions';
+import { useCart } from '@/hooks/useCart';
 
-import { ICartItem } from '@/shared/types/cart.interface'
+import { ICartItem } from '@/shared/types/cart.interface';
 
-import styles from '../HeaderCart.module.scss'
+import styles from '../HeaderCart.module.scss';
 
 interface CartActionsProps {
-	item: ICartItem
+	item: ICartItem;
 }
 
 export function CartActions({ item }: CartActionsProps) {
-	const { changeQuantity } = useActions()
+	const { changeQuantity, removeFromCart } = useActions();
 
-	const { items } = useCart()
-	const quantity = items.find(cartItem => cartItem.id === item.id)?.quantity
+	const { items } = useCart();
+	const quantity = items.find(cartItem => cartItem.id === item.id)?.quantity;
+
+	const handleMinusClick = () => {
+		if (quantity === 1) {
+			removeFromCart({ id: item.id });
+		} else {
+			changeQuantity({ id: item.id, type: 'minus' });
+		}
+	};
 
 	return (
 		<div className={styles.actions}>
-			<Button
-				onClick={() => changeQuantity({ id: item.id, type: 'minus' })}
-				variant='ghost'
-				size='icon'
-				disabled={quantity === 1}
-			>
+			<Button onClick={handleMinusClick} variant="ghost" size="icon">
 				<Minus />
 			</Button>
 
@@ -34,11 +37,11 @@ export function CartActions({ item }: CartActionsProps) {
 
 			<Button
 				onClick={() => changeQuantity({ id: item.id, type: 'plus' })}
-				variant='ghost'
-				size='icon'
+				variant="ghost"
+				size="icon"
 			>
 				<Plus />
 			</Button>
 		</div>
-	)
+	);
 }
