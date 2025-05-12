0742e33c20e9a14d37a1f6d354242d6dab0df7c4
hlumak <vglumakov@gmail.com>
2025-05-12 17:51:07 +0300

    possible fix for vercel deploy

--- src/app/auth/useAuthForm.ts
+++ src/app/auth/useAuthForm.ts
@@ -1,42 +1,45 @@
-import { useMutation } from '@tanstack/react-query'
-import { useRouter } from 'next/navigation'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import toast from 'react-hot-toast'
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
-import { DASHBOARD_URL } from '@/config/url.config'
+import { DASHBOARD_URL } from '@/config/url.config';
 
-import { authService } from '@/services/auth/auth.service'
+import { authService } from '@/services/auth/auth.service';
 
-import { IAuthForm } from '@/shared/types/auth.interface'
+import { IAuthForm } from '@/shared/types/auth.interface';
 
 export function useAuthForm(isReg: boolean) {
-	const router = useRouter()
+	const router = useRouter();
 
 	const form = useForm<IAuthForm>({
 		mode: 'onChange'
-	})
+	});
 
 	const { mutate, isPending } = useMutation({
 		mutationKey: ['auth user'],
 		mutationFn: (data: IAuthForm) =>
 			authService.main(isReg ? 'register' : 'login', data),
 		onSuccess() {
-			form.reset()
-			toast.success('Успішна авторизація')
-			router.replace(DASHBOARD_URL.home())
+			form.reset();
+			toast.success('Успішна авторизація');
+			router.replace(DASHBOARD_URL.home());
+			setTimeout(() => {
+				window.location.href = DASHBOARD_URL.home();
+			}, 100);
 		},
 		onError(error) {
 			if (error.message) {
-				toast.error(error.message)
+				toast.error(error.message);
 			} else {
-				toast.error('Помилка при авторизації')
+				toast.error('Помилка при авторизації');
 			}
 		}
-	})
+	});
 
 	const onSubmit: SubmitHandler<IAuthForm> = data => {
-		mutate(data)
-	}
+		mutate(data);
+	};
 
-	return { onSubmit, form, isPending }
+	return { onSubmit, form, isPending };
 }
