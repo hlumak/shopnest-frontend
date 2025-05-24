f9adcc3a25b85a7f0e9a6d912ab60047b2eacb25
hlumak <vglumakov@gmail.com>
2025-05-24 21:43:56 +0300

    add toast and probably fix for checkout

--- src/store/cart/cart.slice.ts
+++ src/store/cart/cart.slice.ts
@@ -1,14 +1,15 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
 
 import type {
 	IAddToCartPayload,
 	ICartInitialState,
 	IChangeQuantityPayload
-} from './cart.types'
+} from './cart.types';
 
 const initialState: ICartInitialState = {
 	items: []
-}
+};
 
 export const cartSlice = createSlice({
 	name: 'cart',
@@ -17,26 +18,24 @@ export const cartSlice = createSlice({
 		addToCart: (state, action: PayloadAction<IAddToCartPayload>) => {
 			const isExist = state.items.some(
 				item => item.product.id === action.payload.product.id
-			)
+			);
 
 			if (!isExist)
-				state.items.push({ ...action.payload, id: state.items.length })
+				state.items.push({ ...action.payload, id: state.items.length });
+
+			toast.success('Товар додано до кошика');
 		},
 		removeFromCart: (state, action: PayloadAction<{ id: number }>) => {
-			state.items = state.items.filter(
-				item => item.id !== action.payload.id
-			)
+			state.items = state.items.filter(item => item.id !== action.payload.id);
+			toast.success('Товар видалено з кошика');
 		},
-		changeQuantity: (
-			state,
-			action: PayloadAction<IChangeQuantityPayload>
-		) => {
-			const { id, type } = action.payload
-			const item = state.items.find(item => item.id === id)
-			if (item) type === 'plus' ? item.quantity++ : item.quantity--
+		changeQuantity: (state, action: PayloadAction<IChangeQuantityPayload>) => {
+			const { id, type } = action.payload;
+			const item = state.items.find(item => item.id === id);
+			if (item) type === 'plus' ? item.quantity++ : item.quantity--;
 		},
 		reset: state => {
-			state.items = []
+			state.items = [];
 		}
 	}
-})
+});
